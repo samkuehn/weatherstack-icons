8a12e31db7f726d4a8ec4b1b7cbd7acc0d6b6347
Sam Kuehn <samkuehn@gmail.com>
2022-10-19 16:04:41 -0600

    Add make-things

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import Layout from "../components/Layout";
 import WeatherIcon from "../components/WeatherIcon";
 import erikflowers from "../maps/erikflowers.json";
+import makeThings from "../maps/make-things.json";
 
 const IndexPage = () => (
   <Layout title="Weatherstack Icons">
     <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
       <div className="flex flex-col items-center justify-center">
-        <h1 className="mb-2 text-3xl font-bold">Weatherstack Icons</h1>
+        <h1 className="mb-2 text-3xl font-bold">erikflowers</h1>
         <div className="mb-2">
           <a href="https://erikflowers.github.io/weather-icons/">
             https://erikflowers.github.io/weather-icons/
@@ -23,17 +24,25 @@ const IndexPage = () => (
               slug={value.slug}
               description={value.description}
             />
-            // <div className="flex flex-col items-center rounded border border-slate-400 bg-slate-100 py-2 px-1">
-            //   <div>
-            //     <img
-            //       className="h-16 w-16"
-            //       src={`https://raw.githubusercontent.com/erikflowers/weather-icons/bb80982bf1f43f2d57f9dd753e7413bf88beb9ed/svg/wi-${value.slug}.svg`}
-            //     />
-            //   </div>
-            //   <div className="text-sm font-thin">{key}</div>
-            //   <div className="text-xs font-light">{value.slug}</div>
-            //   <div className="text-xs">{value.description}</div>
-            // </div>
+          );
+        })}
+      </div>
+      <div className="my-2">
+        <h1 className="mb-2 text-3xl font-bold">Make-Things</h1>
+        <a href="https://erikflowers.github.io/weather-icons/">
+          https://erikflowers.github.io/weather-icons/
+        </a>
+      </div>
+      <div className="xl:grid-cols- grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-5">
+        {Object.keys(makeThings).map((key, index) => {
+          const value = makeThings[key];
+          return (
+            <WeatherIcon
+              src={`https://raw.githubusercontent.com/Makin-Things/weather-icons/master/animated/${value.slug}.svg`}
+              key={key}
+              slug={value.slug}
+              description={value.description}
+            />
           );
         })}
       </div>
