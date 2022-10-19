1a1b5986f8f26e28f1f87cffd507ba076591be1b
Sam Kuehn <samkuehn@gmail.com>
2022-10-18 19:52:21 -0600

    update node packages

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ const IndexPage = () => (
       {Object.keys(weatherIcons).map((key, index) => {
         const value = weatherIcons[key];
         return (
-          <div className="flex flex-col items-center bg-slate-100">
+          <div className="flex flex-col items-center bg-slate-100 py-2 px-1">
             <div>
               <img
                 className="h-16 w-16"
